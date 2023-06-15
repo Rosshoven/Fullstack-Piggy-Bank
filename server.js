cfbc921dbef271d19985ff92de3a35bf4b55b843
Rosshoven <Michaeledwardross@gmail.com>
2023-06-15 12:34:12 -0500

    getting static files to show

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
+const port = 3000
+
+var cors = require('cors');
 
 
 
-app.use(express.static('public'));
+app.use(express.static('/client/public/index.html'));
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+// app.get('/', (req, res) => {
+//     res.send('hello whore')
+//   })
+
+
+
+
+
+app.listen(port, () => {
+console.log(`Porto Porto Porto ${port}`)
   })
