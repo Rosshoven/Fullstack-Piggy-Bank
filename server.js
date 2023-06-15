16b0bad8e418cc487f2457ace7d4faf252bf9f1b
Rosshoven <Michaeledwardross@gmail.com>
2023-06-15 13:52:18 -0500

    server.js build

--- server.js
+++ server.js
@@ -2,21 +2,51 @@ var express = require('express');
 var app = express();
 const port = 3000;
 
+// const Home = require('./client/src/routes/home');
 
 
 
 
-// app.use(express.static('client/public'));
+
+app.use(express.static('client/public/index.html/'));
 
 
+// Home
 app.get('/', (req, res) => {
-    res.send('hello whore')
-  })
+    res.send('HOME! Where my feet feel fucked up, HOME!');
+});
+
+// Create Account
+app.get('/create-account', (req, res) => {
+    res.send('Create Account Form');
+});
+
+// Login
+app.get('/login', (req, res) => {
+    res.send('Login Form');
+});
+
+// Deposit
+app.get('/deposit', (req, res) => {
+    res.send('Deposit');
+});
+
+// Withdraw
+app.get('/withdraw', (req, res) => {
+    res.send('Withdraw');
+});
+
+// Balance
+app.get('/balance', (req, res) => {
+    res.send('Balance');
+});
+
+app.get('/all-data', (req, res) => {
+    res.send('All Data');
+})
 
 
-
-
-
+// connecting port 
 app.listen(port, () => {
 console.log(`Porto Porto Porto ${port}`)
-  })
+  });
